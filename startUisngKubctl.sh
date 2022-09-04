feb1f4dd1e8356a0fa2eab636c1f516b61e904b0
vinayak <itssvinayak@gmail.com>
2022-09-04 22:57:48 +0530

    configMap and env

--- startUisngKubctl.sh
+++ startUisngKubctl.sh
@@ -1,3 +1,9 @@
-kubectl apply -f app-depolyment.yaml -f app-service.yaml -f redis-service.yaml -f redis-deployment.yaml
+kubectl apply                                    \
+    -f app-depolyment.yaml                       \
+    -f app-service.yaml                          \
+    -f redis-service.yaml                        \
+    -f redis-deployment.yaml                     \
+    -f redis-hostPathPersistentVolumeClaim.yaml  \
+    -f redis-hostPathPersistentVolume.yaml          
 
 # apply the deployment and service for the app
